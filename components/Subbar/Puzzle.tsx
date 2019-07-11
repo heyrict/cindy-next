bdde3c33afdafc7efdac00690c0c39977f79785c
heyrict <xiezh0831@yahoo.co.jp>
2019-07-12 06:20:02 +0800

    PuzzleSubbar: change styles

--- components/Subbar/Puzzle.tsx
+++ components/Subbar/Puzzle.tsx
@@ -20,70 +20,75 @@ const ButtonTransparentA = ButtonTransparent.withComponent('a');
 
 const PuzzleSubbar = ({ route }: PuzzleSubbarProps) => (
   <Flex flexWrap="wrap" mb={3}>
-    <Box
-      width={[1 / 3, 1 / 5, 1 / 6, 1 / 8]}
-      bg={route === '/puzzles' ? 'yellow.4' : 'orange.4'}
-      borderRadius={2}
-      mx={1}
-      p={1}
-    >
-      <Link to="puzzles" prefetch passHref>
-        <ButtonTransparentA color="red.9" height={1} width={1}>
-          <FormattedMessage {...puzzlesMessage.header} />
-        </ButtonTransparentA>
-      </Link>
+    <Box width={[1 / 3, 1 / 5, 1 / 6, 1 / 8]} mb={1}>
+      <Box
+        bg={route === '/puzzles' ? 'yellow.4' : 'orange.4'}
+        borderRadius={2}
+        ml={1}
+        p={1}
+      >
+        <Link to="puzzles" prefetch passHref>
+          <ButtonTransparentA color="red.9" height={1} width={1}>
+            <FormattedMessage {...puzzlesMessage.header} />
+          </ButtonTransparentA>
+        </Link>
+      </Box>
     </Box>
-    <Box
-      width={[1 / 3, 1 / 5, 1 / 6, 1 / 8]}
-      bg={route === '/add/puzzle' ? 'yellow.4' : 'orange.4'}
-      borderRadius={2}
-      mr={1}
-      p={1}
-    >
-      <Link to="add/puzzle" prefetch passHref>
-        <ButtonTransparentA color="red.9" height={1} width={1}>
-          <FormattedMessage {...addPuzzleMessage.header} />
-        </ButtonTransparentA>
-      </Link>
+    <Box width={[1 / 3, 1 / 5, 1 / 6, 1 / 8]} mb={1}>
+      <Box
+        bg={route === '/add/puzzle' ? 'yellow.4' : 'orange.4'}
+        borderRadius={2}
+        ml={1}
+        p={1}
+      >
+        <Link to="add/puzzle" prefetch passHref>
+          <ButtonTransparentA color="red.9" height={1} width={1}>
+            <FormattedMessage {...addPuzzleMessage.header} />
+          </ButtonTransparentA>
+        </Link>
+      </Box>
     </Box>
-    <Box
-      width={[1 / 3, 1 / 5, 1 / 6, 1 / 8]}
-      bg={route === '/ranking' ? 'yellow.4' : 'orange.4'}
-      borderRadius={2}
-      mr={1}
-      p={1}
-    >
-      <Link to="ranking" prefetch passHref>
-        <ButtonTransparentA color="red.9" height={1} width={1}>
-          <FormattedMessage {...rankingMessage.header} />
-        </ButtonTransparentA>
-      </Link>
+    <Box width={[1 / 3, 1 / 5, 1 / 6, 1 / 8]} mb={1}>
+      <Box
+        bg={route === '/ranking' ? 'yellow.4' : 'orange.4'}
+        borderRadius={2}
+        ml={1}
+        p={1}
+      >
+        <Link to="ranking" prefetch passHref>
+          <ButtonTransparentA color="red.9" height={1} width={1}>
+            <FormattedMessage {...rankingMessage.header} />
+          </ButtonTransparentA>
+        </Link>
+      </Box>
     </Box>
-    <Box
-      width={[1 / 3, 1 / 5, 1 / 6, 1 / 8]}
-      bg={route === '/awards' ? 'yellow.4' : 'orange.4'}
-      borderRadius={2}
-      mr={1}
-      p={1}
-    >
-      <Link to="awards" prefetch passHref>
-        <ButtonTransparentA color="red.9" height={1} width={1}>
-          <FormattedMessage {...awardsMessages.header} />
-        </ButtonTransparentA>
-      </Link>
+    <Box width={[1 / 3, 1 / 5, 1 / 6, 1 / 8]} mb={1}>
+      <Box
+        bg={route === '/awards' ? 'yellow.4' : 'orange.4'}
+        borderRadius={2}
+        ml={1}
+        p={1}
+      >
+        <Link to="awards" prefetch passHref>
+          <ButtonTransparentA color="red.9" height={1} width={1}>
+            <FormattedMessage {...awardsMessages.header} />
+          </ButtonTransparentA>
+        </Link>
+      </Box>
     </Box>
-    <Box
-      width={[1 / 3, 1 / 5, 1 / 6, 1 / 8]}
-      bg={route === '/search' ? 'yellow.4' : 'orange.4'}
-      borderRadius={2}
-      mr={1}
-      p={1}
-    >
-      <Link to="search" prefetch passHref>
-        <ButtonTransparentA color="red.9" height={1} width={1}>
-          <FormattedMessage {...searchMessages.header} />
-        </ButtonTransparentA>
-      </Link>
+    <Box width={[1 / 3, 1 / 5, 1 / 6, 1 / 8]} mb={1}>
+      <Box
+        bg={route === '/search' ? 'yellow.4' : 'orange.4'}
+        borderRadius={2}
+        ml={1}
+        p={1}
+      >
+        <Link to="search" prefetch passHref>
+          <ButtonTransparentA color="red.9" height={1} width={1}>
+            <FormattedMessage {...searchMessages.header} />
+          </ButtonTransparentA>
+        </Link>
+      </Box>
     </Box>
   </Flex>
 );
