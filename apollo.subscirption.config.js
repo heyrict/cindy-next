b875c558c52d35123d4efd74f3de46636f63887e
heyrict <xiezh0831@yahoo.co.jp>
2019-07-15 15:41:28 +0800

    update apollo config

--- apollo.subscirption.config.js
+++ apollo.subscirption.config.js
@@ -2,7 +2,7 @@ module.exports = {
   client: {
     service: {
       name: 'hasura-trigger-subscription',
-      url: 'http://localhost:3000/subscriptions',
+      url: 'http://localhost:3001/subscriptions',
     },
     includes: [
       './graphql/Fragments/**',
