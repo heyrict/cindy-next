59d82cfe8816d22e66dea2a63fc0a8c9613650b3
heyrict <xiezh0831@yahoo.co.jp>
2019-07-25 13:39:53 +0800

    Add cache to active users request

--- server/controllers/activeUsers.js
+++ server/controllers/activeUsers.js
@@ -7,7 +7,18 @@ const getAPIURL = params =>
     .map(([k, v]) => `${k}=${v}`)
     .join('&')}`;
 
+const activeUsersCache = new Object({ time: -1, data: null });
+
+const CACHE_TIME = 5 * 60 * 1000; // 5 minutes;
+
 export const postActiveUsers = async (req, res) => {
+  // Ignore request body as we only have one set of parameters.
+  const now = new Date();
+  if (now.getTime() - activeUsersCache.time < CACHE_TIME) {
+    res.status(200).send(activeUsersCache.data);
+    return;
+  }
+
   try {
     const params = req.body;
     fetch(getAPIURL(params), {
@@ -18,6 +29,8 @@ export const postActiveUsers = async (req, res) => {
     })
       .then(res => res.json())
       .then(data => {
+        activeUsersCache.time = now.getTime();
+        activeUsersCache.data = data;
         res.status(200).send(data);
       });
   } catch (e) {
--- settings.js
+++ settings.js
@@ -68,5 +68,5 @@ export const domainFilter = url => {
 };
 
 export const SCRIPTS = [
-  '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");ym(54573919, "init", {clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});',
+  '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");ym(54573919,"init",{clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});',
 ];
