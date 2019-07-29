84cd67677ad8181b07d3b7a01b3b0fb5f2a6b31e
heyrict <xiezh0831@yahoo.co.jp>
2019-07-29 18:13:09 +0800

    Add puzzle insert callback

--- docker-run.sh
+++ docker-run.sh
@@ -4,6 +4,7 @@ docker run -d --net=host --name hasura-cindy-next \
        -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
        -e HASURA_GRAPHQL_CONSOLE_ASSETS_DIR=/srv/console-assets \
        -e SUBSCRIPTION_SERVER_WEBHOOK="http://localhost:3001/webhook/subscriptions" \
+       -e PYTHON_SERVER_WEBHOOK="http://localhost:3002/webhook" \
        -e HASURA_GRAPHQL_JWT_SECRET='{
           "type": "RS256",
           "jwk_url": "http://localhost:3001/webhook/jwks",
---
+++ python/templates.py
@@ -0,0 +1,10 @@
+class en:
+    ADD_PUZZLE_TWEET = ''
+
+
+class ja:
+    ADD_PUZZLE_TWEET = \
+        '%(user_nickname)sさんがCindyにて新しい問題'\
+        '『%(title)s』を出題しました。\n'\
+        'https://www.cindythink.com/puzzle/show/%(id)d\n'\
+        '#ウミガメのスープ'
