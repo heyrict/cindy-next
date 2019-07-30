f0c1a2fdceaee9eff472d92e58ef4bcd011ba4da
heyrict <xiezh0831@yahoo.co.jp>
2019-07-30 18:24:21 +0800

    Purge slate editor

--- components/PreviewEditor/index.ts
+++ components/PreviewEditor/index.ts
@@ -1,5 +1,2 @@
-export { default as SimpleSlateEditor } from './Slate/simple';
-export { default as SlateEditor } from './Slate/editor';
-
 export { default as LegacyEditor } from './Legacy/editor';
 export { default as SimpleLegacyEditor } from './Legacy/simple';
