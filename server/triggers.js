a7e9ccefc51a782c0e105137fa6249a4bbe0e328
heyrict <xiezh0831@yahoo.co.jp>
2020-01-30 12:48:10 +0800

    chore: Remove subscription from server

--- server/triggers.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  ON_CHATMESSAGE_CHANGE: 'on_chatmessage_change',
-  ON_DIALOGUE_CHANGE: 'on_dialogue_change',
-  ON_DIRECTMESSAGE_CHANGE: 'on_directmessage_change',
-  ON_HINT_CHANGE: 'on_hint_change',
-  ON_PUZZLE_CHANGE: 'on_puzzle_change',
-};
