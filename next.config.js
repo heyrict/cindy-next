3c3bc679899c593da5581c1d60e2d8a69c010efd
heyrict <xiezh0831@yahoo.co.jp>
2020-02-29 09:24:44 +0800

    First attempt to switch to swc

--- next.config.js
+++ next.config.js
@@ -1,22 +1,29 @@
 module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.modules.push('.');
-    config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader',
-    });
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            outputPath: 'static/_files',
-            publicPath: '/_next/static/_files',
+    config.modules.rules = [
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: /node_modules/,
+        use: 'swc-loader',
+      },
+      {
+        test: /\.md$/,
+        use: 'raw-loader',
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'static/_files',
+              publicPath: '/_next/static/_files',
+            },
           },
-        },
-      ],
-    });
+        ],
+      },
+    ];
     return config;
   },
   experimental: { publicDirectory: true },
