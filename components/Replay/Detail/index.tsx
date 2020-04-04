0080ffa48fcbba38566ccaa567f65ff7feebe246
heyrict <xiezh0831@yahoo.co.jp>
2020-04-04 17:13:37 +0800

    feat: Add replay icon to title

--- components/Replay/Detail/index.tsx
+++ components/Replay/Detail/index.tsx
@@ -4,13 +4,18 @@ import { ReplayDetailProps } from 'components/Replay/Detail/types';
 
 import Box from 'components/General/Box';
 import Flex from 'components/General/Flex';
+import Img from 'components/General/Img';
 import { PuzzleTitleBase } from 'components/Puzzle/Detail/PuzzleTitle';
 import ContentsFrame from 'components/Puzzle/Detail/ContentsFrame';
 import ReplayPlay from './ReplayPlay';
+import replayIcon from 'svgs/puzzleDetailReplay.svg';
 
 const ReplayDetail = ({ replay }: ReplayDetailProps) => (
-  <Flex flexWrap="wrap" justifyContent="center">
-    <PuzzleTitleBase>{replay.title}</PuzzleTitleBase>
+  <Flex flexWrap="wrap" justifyContent="center" alignItems="center">
+    <PuzzleTitleBase>
+      <Img height="sm" mr={2} src={replayIcon} />
+      {replay.title}
+    </PuzzleTitleBase>
     <ContentsFrame
       text={replay.sui_hei_puzzle ? replay.sui_hei_puzzle.content : ''}
       user={replay.sui_hei_user}
