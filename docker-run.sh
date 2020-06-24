8ac6f303a9aa744123355082df07ec57de67c79f
heyrict <xiezh0831@yahoo.co.jp>
2020-06-25 06:40:18 +0800

    chore: Update docker-run commands

--- docker-run.sh
+++ docker-run.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 docker run -d --net=host --name hasura-cindy-next \
-       -e HASURA_GRAPHQL_DATABASE_URL=postgres://cindy:cindy@localhost:5432/cindy \
-       -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
-       -e HASURA_GRAPHQL_CONSOLE_ASSETS_DIR=/srv/console-assets \
-       -e SUBSCRIPTION_SERVER_WEBHOOK="http://localhost:3001/webhook/subscriptions" \
-       -e PYTHON_SERVER_WEBHOOK="http://localhost:3002/webhook" \
-       -e HASURA_GRAPHQL_JWT_SECRET='{
-          "type": "RS256",
-          "jwk_url": "http://localhost:3001/webhook/jwks",
-          "claims_namespace": "https://www.cindythink.com/jwt/claims",
-          "claims_format": "json"
-       }' \
-       -e HASURA_GRAPHQL_ADMIN_SECRET="CINDYTHINK_HASURA_ADMIN_SECRET" \
-       -e HASURA_GRAPHQL_UNAUTHORIZED_ROLE=anonymous \
-       hasura/graphql-engine:latest
+        --log-opt max-size=10m \
+        -e HASURA_GRAPHQL_DATABASE_URL=postgres://cindy:cindy@localhost:5432/cindy \
+        -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
+        -e HASURA_GRAPHQL_CONSOLE_ASSETS_DIR=/srv/console-assets \
+        -e SUBSCRIPTION_SERVER_WEBHOOK="http://localhost:3001/webhook/subscriptions" \
+        -e PYTHON_SERVER_WEBHOOK="http://localhost:3002/webhook" \
+        -e HASURA_GRAPHQL_JWT_SECRET='{
+           "type": "RS256",
+           "jwk_url": "http://localhost:3001/webhook/jwks",
+           "claims_namespace": "https://www.cindythink.com/jwt/claims",
+           "claims_format": "json"
+        }' \
+        -e HASURA_GRAPHQL_ADMIN_SECRET="CINDYTHINK_HASURA_ADMIN_SECRET" \
+        -e HASURA_GRAPHQL_UNAUTHORIZED_ROLE=anonymous \
+        hasura/graphql-engine:latest
